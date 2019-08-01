cccbc882880e643987822e7cf07b216973d71a7d
xueyilu <934882591@qq.com>
2019-08-01 19:56:07 +0800

    学完画线和填充块明天学颜色

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="hello">
+    <canvas id="tutorial" width="150" height="200">不支持canvas</canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.draw();
+    },
+    draw() {
+      var canvas = document.getElementById("tutorial");
+      if (canvas.getContext) {
+        var ctx = canvas.getContext("2d");
+        // drawing code here
+        this.drawHeart(ctx);
+      }
+    },
+    drawRect1(ctx) {
+      ctx.fillStyle = "rgb(200,0,0)";
+      ctx.fillRect(10, 10, 55, 50);
+      ctx.fillStyle = "rgba(0,0,200,0.5)";
+      ctx.fillRect(30, 30, 55, 50);
+    },
+    drawRect2(ctx) {
+      ctx.fillRect(25, 25, 100, 100);
+      ctx.clearRect(45, 45, 60, 60);
+      ctx.strokeRect(50, 50, 50, 50);
+    },
+    drawPath1(ctx) {
+      ctx.beginPath();
+      ctx.moveTo(75, 50);
+      ctx.lineTo(100, 75);
+      ctx.lineTo(100, 25);
+      ctx.fill();
+    },
+    drawSmile(ctx) {
+      ctx.beginPath();
+      ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
+      ctx.moveTo(110, 75);
+      ctx.arc(75, 75, 35, 0, Math.PI, false);
+      ctx.moveTo(65, 65);
+      ctx.arc(60, 65, 5, 0, Math.PI * 2, true);
+      ctx.moveTo(95, 65);
+      ctx.arc(90, 65, 5, 0, Math.PI * 2, true);
+      ctx.stroke();
+    },
+    drawLine(ctx) {
+      ctx.beginPath();
+      ctx.moveTo(25, 25);
+      ctx.lineTo(105, 25);
+      ctx.lineTo(25, 105);
+      ctx.fill();
+
+      ctx.beginPath();
+      ctx.moveTo(125, 125);
+      ctx.lineTo(125, 45);
+      ctx.lineTo(45, 125);
+      ctx.closePath();
+      ctx.stroke();
+    },
+    drawArc(ctx) {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 3; j++) {
+          ctx.beginPath();
+          let x = 25 + j * 50;
+          let y = 25 + i * 50;
+          let radius = 20;
+          let startAngle = 0;
+          let endAngle = Math.PI + (Math.PI * j) / 2;
+          let anticlockwise = i % 2 == 0 ? false : true;
+          ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+          if (i > 1) {
+            ctx.fill();
+          } else {
+            ctx.stroke();
+          }
+        }
+      }
+    },
+    drawBubble(ctx) {
+      ctx.beginPath();
+      ctx.moveTo(75, 25);
+      ctx.quadraticCurveTo(25, 25, 25, 62.5);
+      ctx.quadraticCurveTo(25, 100, 50, 100);
+      ctx.quadraticCurveTo(50, 120, 30, 125);
+      ctx.quadraticCurveTo(60, 120, 65, 100);
+      ctx.quadraticCurveTo(125, 100, 125, 62.5);
+      ctx.quadraticCurveTo(125, 25, 75, 25);
+      ctx.stroke();
+    },
+    drawHeart(ctx) {
+      ctx.beginPath();
+      ctx.moveTo(75, 40);
+      ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+      ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+      ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+      ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+      ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+      ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+      ctx.fill();
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+canvas {
+  border: 1px solid black;
+}
+</style>
